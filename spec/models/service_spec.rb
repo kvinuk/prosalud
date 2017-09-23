34b371d7ad00e691d73dc0e565ee7495e7b29f57
Jc <jcou@hotmail.com>
2017-09-23 17:53:59 -0500

    Service Specs

--- spec/models/service_spec.rb
+++ spec/models/service_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Service, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:service) {FactoryGirl.build(:service)}
+  subject {service}
+
+  it { should have_many(:treatments) }
+  it { should validate_presence_of(:name) }
+  it { should validate_presence_of(:initial_price) }
+  it { should validate_presence_of(:subsequent_price) }
+  it { should validate_presence_of(:community_price) }
+  #it { should validate_numericality_of(:initial_price).only_float }
+  #it { should validate_numericality_of(:subsequent_price).only_float }
+  #it { should validate_numericality_of(:community_price).only_float }
+
+  
+  it { should validate_numericality_of(:initial_price).is_grater_than_or_equal_to(0) }
+  it { should validate_numericality_of(:subsequent_price).is_grater_than_or_equal_to(0)}
+  it { should validate_numericality_of(:community_price).is_grater_than_or_equal_to(0)}
+  
+
+
 end
