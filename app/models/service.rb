f3d0e89b228d5c93eeb1b8bb5627c08d31ab9d6e
Jc <jcou@hotmail.com>
2017-09-23 18:13:18 -0500

    Service update

--- app/models/service.rb
+++ app/models/service.rb
@@ -1,15 +1,11 @@
-<<<<<<< HEAD
 class Service < ApplicationRecord
 	validates :name, :initial_price, :subsequent_price, :community_price, presence: true	
 	has_many :treatments
-	validates_numericality_of :initial_price, grater_than_or_equal_to: 0
-	validates_numericality_of :subsequent_price, grater_than_or_equal_to: 0
-	validates_numericality_of :community_price, grater_than_or_equal_to: 0
+	validates_numericality_of :initial_price, greater_than_or_equal_to: 0
+	validates_numericality_of :subsequent_price, greater_than_or_equal_to: 0
+	validates_numericality_of :community_price, greater_than_or_equal_to: 0
 	validates_numericality_of :initial_price, only_float: true
-	validates_numericality_of :subsequent_price, only_float: true
-	validates_numericality_of :community_price, only_float: true
-=======
-class Service < ApplicationRecord 
-  has_many :treatments
->>>>>>> faeb057b485de3c27c9210afc0c77c9b0f81edc1
+	validates_numericality_of :subsequent_price
+	validates_numericality_of :community_price
+
 end
--- spec/models/service_spec.rb
+++ spec/models/service_spec.rb
@@ -9,14 +9,12 @@ RSpec.describe Service, type: :model do
   it { should validate_presence_of(:initial_price) }
   it { should validate_presence_of(:subsequent_price) }
   it { should validate_presence_of(:community_price) }
-  #it { should validate_numericality_of(:initial_price).only_float }
-  #it { should validate_numericality_of(:subsequent_price).only_float }
-  #it { should validate_numericality_of(:community_price).only_float }
-
-  
-  it { should validate_numericality_of(:initial_price).is_grater_than_or_equal_to(0) }
-  it { should validate_numericality_of(:subsequent_price).is_grater_than_or_equal_to(0)}
-  it { should validate_numericality_of(:community_price).is_grater_than_or_equal_to(0)}
+  it { should validate_numericality_of(:initial_price) }
+  it { should validate_numericality_of(:subsequent_price) }
+  it { should validate_numericality_of(:community_price) }
+  it { should validate_numericality_of(:initial_price).is_greater_than_or_equal_to(0) }
+  it { should validate_numericality_of(:subsequent_price).is_greater_than_or_equal_to(0)}
+  it { should validate_numericality_of(:community_price).is_greater_than_or_equal_to(0)}
   
 
 
