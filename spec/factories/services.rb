4003fdc667fe0a873f18760981acb211b463aaf0
Jc <jcou@hotmail.com>
2017-09-23 14:52:09 -0500

    Model and Factory update

--- spec/factories/services.rb
+++ spec/factories/services.rb
@@ -1,5 +1,9 @@
 FactoryGirl.define do
   factory :service do
+  	name {FFaker::NameMX.full_name}
+  	initial_price {((rand + 1)*100).round(2)}
+  	subsequent_price {((rand + 1)*100).round(2)}
+  	community_price {((rand + 1)*100).round(2)}
     
   end
 end
