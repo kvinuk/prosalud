016e07e46f3c6a01f5a42761fb8252bdede0643e
kvinuk <kevinomuro@gmail.com>
2017-09-23 18:03:26 -0500

    Fixes rubocop offences on service

--- app/models/service.rb
+++ app/models/service.rb
@@ -1,15 +1,10 @@
-<<<<<<< HEAD
 class Service < ApplicationRecord
-	validates :name, :initial_price, :subsequent_price, :community_price, presence: true	
-	has_many :treatments
-	validates_numericality_of :initial_price, grater_than_or_equal_to: 0
-	validates_numericality_of :subsequent_price, grater_than_or_equal_to: 0
-	validates_numericality_of :community_price, grater_than_or_equal_to: 0
-	validates_numericality_of :initial_price, only_float: true
-	validates_numericality_of :subsequent_price, only_float: true
-	validates_numericality_of :community_price, only_float: true
-=======
-class Service < ApplicationRecord 
+  validates :name, :initial_price, :subsequent_price, :community_price, presence: true  
   has_many :treatments
->>>>>>> faeb057b485de3c27c9210afc0c77c9b0f81edc1
+  validates_numericality_of :initial_price, grater_than_or_equal_to: 0
+  validates_numericality_of :subsequent_price, grater_than_or_equal_to: 0
+  validates_numericality_of :community_price, grater_than_or_equal_to: 0
+  validates_numericality_of :initial_price, only_float: true
+  validates_numericality_of :subsequent_price, only_float: true
+  validates_numericality_of :community_price, only_float: true
 end
