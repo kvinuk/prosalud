62ff8125cdd126068a61335e0672883f677860ce
Ricardo <ricardotrevino24@gmail.com>
2017-09-23 20:16:33 -0500

    adds Treatments controller and view

---
+++ app/controllers/treatments_controller.rb
@@ -0,0 +1,54 @@
+class TreatmentsController < ApplicationController
+  before_action :set_treatment, only: [:edit, :update, :show, :destroy]
+  load_and_authorize_resource
+
+  def index
+    @treatments = Treatment.all
+  end
+
+  def show
+  end
+
+  def new
+    @treatment = Treatment.new
+    @clients = Client.all
+    @services = Service.all
+    @therapists = Therapist.all
+  end
+
+  def create
+    @treatment = Treatment.new(treatment_params)
+    if @treatment.save
+      redirect_to treatments_path
+    else
+      flash.now[:danger] = @treatment.errors.full_messages
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @treatment.update(treatment_params)
+      redirect_to new_treatment_path
+    else
+      flash.now[:danger] = @treatment.errors.full_messages
+      render :edit
+    end
+  end
+
+  def destroy
+    @treatment.destroy
+  end
+
+  private
+
+  def treatment_params
+    params.require(:treatment).permit(:start_date, :end_date, :client, :therapist, :service)
+  end
+
+  def set_treatment
+    @treatment = Treatment.find(params[:id])
+  end
+end
