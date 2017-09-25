ee41d291f9a0322bd6946cd4d24ee81f9a9cbef7
Kevin Muro <kevinomuro@gmail.com>
2017-09-25 14:54:55 -0500

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
   end
 
   def after_sign_in_path_for(resource_or_scope)
-    channels_path
+    clients_path
   end
 
   def after_sign_out_path_for(resource_or_scope)
